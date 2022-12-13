b92c260d6eccb1ddf064bc3eeac95043ccf7bcbb
Vladimir Valeyev <ValV@users.noreply.github.com>
2022-12-13 20:18:00 +0300

    Handle import errors

--- maritimeai/__init__.py
+++ maritimeai/__init__.py
@@ -1,9 +1,12 @@
-from .geotiffs import read_to_channels
-from .geotiffs import enhance_dataset
-from .geotiffs import save_grayscale
-from .geotiffs import cluster_dataset
-from .geotiffs import vectorize_dataset
+try:
+    from .geotiffs import read_to_channels
+    from .geotiffs import enhance_dataset
+    from .geotiffs import save_grayscale
+    from .geotiffs import cluster_dataset
+    from .geotiffs import vectorize_dataset
 
-from .geotiffs import CHANNELS_RGB, CHANNELS_RGBA
+    from .geotiffs import CHANNELS_RGB, CHANNELS_RGBA
 
-from .processing import process_sentinel1
+    from .processing import process_sentinel1
+except ModuleNotFoundError:
+    pass
